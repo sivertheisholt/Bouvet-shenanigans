23554d76280866c5f9234ccd3f649b35ea3fe872
sivertheisholt <sivert.b.heisholt@protonmail.com>
2023-08-09 09:34:39 +0200

    added service worker

--- Bouvet-Shenanigans.Web/src/index.tsx
+++ Bouvet-Shenanigans.Web/src/index.tsx
@@ -3,7 +3,42 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { App } from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const root = ReactDOM.createRoot(document.getElementById('root')!);
+
+const check = () => {
+  if (!("serviceWorker" in navigator)) {
+    throw new Error("No Service Worker support!");
+  }
+  if (!("PushManager" in window)) {
+    throw new Error("No Push API Support!");
+  }
+};
+
+const registerServiceWorker = async () => {
+  const swRegistration = await navigator.serviceWorker.register("service-worker.js");
+  return swRegistration;
+};
+
+const requestNotificationPermission = async () => {
+  const permission = await window.Notification.requestPermission();
+  // value of permission can be 'granted', 'default', 'denied'
+  // granted: user has accepted the request
+  // default: user has dismissed the notification permission popup by clicking on x
+  // denied: user has denied the request.
+  if (permission !== "granted") {
+    throw new Error("Permission not granted for Notification");
+  }
+};
+
+const main = async () => {
+  check();
+  const permission = await requestNotificationPermission();
+  const swRegistration = await registerServiceWorker();
+};
+
+main()
+
 root.render(
   <React.StrictMode>
     <App />
