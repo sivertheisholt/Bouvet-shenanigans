e167f3f4441f5bd8ac039daebd400be0b24083ab
sivertheisholt <sivert.b.heisholt@protonmail.com>
2023-08-10 11:57:53 +0200

    Log

--- Bouvet-Shenanigans.Web/src/index.tsx
+++ Bouvet-Shenanigans.Web/src/index.tsx
@@ -17,6 +17,7 @@ const check = () => {
 
 const registerServiceWorker = async () => {
   const swRegistration = await navigator.serviceWorker.register("/service-worker.js");
+  console.log(swRegistration.active)
   return swRegistration;
 };
 
