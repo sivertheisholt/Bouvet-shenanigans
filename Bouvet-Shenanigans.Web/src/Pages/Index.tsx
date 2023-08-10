9cc61bc2b06beb688aa565b7aba4bc65d308276a
sivertheisholt <sivert.b.heisholt@protonmail.com>
2023-08-10 11:06:55 +0200

    Asking for notification permission

--- Bouvet-Shenanigans.Web/src/Pages/Index.tsx
+++ Bouvet-Shenanigans.Web/src/Pages/Index.tsx
@@ -1,10 +1,27 @@
 import React from "react"
 import NavComponent from "../Components/Nav/NavComponent"
 
+function askPermission() {
+    return new Promise(function (resolve, reject) {
+      const permissionResult = Notification.requestPermission(function (result) {
+        resolve(result);
+      });
+  
+      if (permissionResult) {
+        permissionResult.then(resolve, reject);
+      }
+    }).then(function (permissionResult) {
+      if (permissionResult !== 'granted') {
+        throw new Error("We weren't granted permission.");
+      }
+    });
+  }
+
 const IndexComponent = () => {
     return (
         <div>
             <NavComponent/>
+            <button style={{padding: "20px"}} onClick={askPermission}>Ask for permission</button>
         </div>
     )
 }
