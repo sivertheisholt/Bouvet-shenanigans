1833857790dae438367fbc4ea65e08582819e94a
sivertheisholt <sivert.b.heisholt@protonmail.com>
2023-08-10 12:12:37 +0200

    Trying new structure

--- Bouvet-Shenanigans.Web/public/service-worker.js
+++ Bouvet-Shenanigans.Web/public/service-worker.js
@@ -1,7 +1,38 @@
 /* eslint-disable no-restricted-globals */
 
-// urlB64ToUint8Array is a magic function that will encode the base64 public key
-// to Array buffer which is needed by the subscription option
+self.addEventListener("install", (event) => {
+  console.log("Running install event");
+  event.waitUntil(self.registration?.navigationPreload.enable());
+  self.skipWaiting()
+});
+
+self.addEventListener("activate", async () => {
+  try {
+    console.log("Service worker activated");
+    const applicationServerKey = urlB64ToUint8Array(
+      "BOoQqpt5TXsZP0Ms1pzu4MIVsGld2uXzcvcOuppsweBM67yti1zog6Qr9dnnSKIHcr_L-29U3dqr7rUG8a_XotI"
+    );
+    const options = { applicationServerKey, userVisibleOnly: true };
+    const subscription = await self.registration.pushManager.subscribe(options);
+    console.log(JSON.stringify(subscription));
+    const response = await saveSubscription(subscription);
+    console.log("Done");
+    console.log(response);
+  } catch (err) {
+    console.log("Error", err);
+  }
+});
+
+self.addEventListener("push", function (event) {
+  if (event.data) {
+    self.registration.showNotification("Hello from bouvet shenanigans");
+    console.log("Push event!! ", event.data.text());
+  } else {
+    self.registration.showNotification("Not payload");
+    console.log("Push event but no data");
+  }
+});
+
 const urlB64ToUint8Array = (base64String) => {
   const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
   const base64 = (base64String + padding)
@@ -31,34 +62,3 @@ const saveSubscription = async (subscription) => {
   return response.json();
 };
 
-self.addEventListener("install", (e) => {
-  console.log(e.type);
-  self.skipWaiting(); // always activate updated SW immediately
-});
-
-self.addEventListener("activate", async () => {
-  try {
-    console.log("Service worker activated");
-    const applicationServerKey = urlB64ToUint8Array(
-      "BOoQqpt5TXsZP0Ms1pzu4MIVsGld2uXzcvcOuppsweBM67yti1zog6Qr9dnnSKIHcr_L-29U3dqr7rUG8a_XotI"
-    );
-    const options = { applicationServerKey, userVisibleOnly: true };
-    const subscription = await self.registration.pushManager.subscribe(options);
-    console.log(JSON.stringify(subscription));
-    const response = await saveSubscription(subscription);
-    console.log("Done");
-    console.log(response);
-  } catch (err) {
-    console.log("Error", err);
-  }
-});
-
-self.addEventListener("push", function (event) {
-  if (event.data) {
-    self.registration.showNotification("Hello from bouvet shenanigans");
-    console.log("Push event!! ", event.data.text());
-  } else {
-    self.registration.showNotification("Not payload");
-    console.log("Push event but no data");
-  }
-});
--- Bouvet-Shenanigans.Web/src/index.tsx
+++ Bouvet-Shenanigans.Web/src/index.tsx
@@ -6,30 +6,6 @@ import { App } from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 
-const check = () => {
-  if (!("serviceWorker" in navigator)) {
-    throw new Error("No Service Worker support!");
-  }
-  if (!("PushManager" in window)) {
-    throw new Error("No Push API Support!");
-  }
-};
-
-const registerServiceWorker = async () => {
-  const swRegistration = await navigator.serviceWorker.register("/service-worker.js");
-  console.log(swRegistration.active)
-  return swRegistration;
-};
-
-
-
-const main = async () => {
-  check();
-  console.log("Registrering service worker")
-  await registerServiceWorker();
-};
-
-main()
 
 root.render(
   <React.StrictMode>
