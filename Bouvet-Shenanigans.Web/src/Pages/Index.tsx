85293eaf799ce13eb9eca2465b22b70518886b89
sivertheisholt <sivert.b.heisholt@protonmail.com>
2023-08-17 14:04:00 +0200

    Fixed name

--- Bouvet-Shenanigans.Web/src/Pages/Index.tsx
+++ Bouvet-Shenanigans.Web/src/Pages/Index.tsx
@@ -1,59 +1,60 @@
-import React from "react"
-import NavComponent from "../Components/Nav/NavComponent"
+import React from "react";
+import NavComponent from "../Components/Nav/NavComponent";
 
 function askPermission() {
-    return new Promise(function (resolve, reject) {
-
-        const permissionResult = Notification.requestPermission(function (result) {
-            resolve(result);
-        });
-
-        if (permissionResult) {
-            permissionResult.then(resolve, reject);
-        }
-    }).then(function (permissionResult) {
-      if (permissionResult !== 'granted') {
-        throw new Error("We weren't granted permission.");
-      }
+  return new Promise(function (resolve, reject) {
+    const permissionResult = Notification.requestPermission(function (result) {
+      resolve(result);
     });
+
+    if (permissionResult) {
+      permissionResult.then(resolve, reject);
+    }
+  }).then(function (permissionResult) {
+    if (permissionResult !== "granted") {
+      throw new Error("We weren't granted permission.");
+    }
+  });
 }
 const registerServiceWorker = async () => {
-    if ("serviceWorker" in navigator) {
-      if (("PushManager" in window)) {
-        try {
-            const registration = await navigator.serviceWorker.register("/sw.js");
+  if ("serviceWorker" in navigator) {
+    if ("PushManager" in window) {
+      try {
+        const registration = await navigator.serviceWorker.register(
+          "/notification-worker.js"
+        );
         if (registration.installing) {
-            console.log("Service worker installing");
+          console.log("Service worker installing");
         } else if (registration.waiting) {
-            console.log("Service worker installed");
+          console.log("Service worker installed");
         } else if (registration.active) {
-            console.log("Service worker active");
+          console.log("Service worker active");
         }
-        } catch (error) {
-            console.error(`Registration failed with ${error}`);
-        }
+      } catch (error) {
+        console.error(`Registration failed with ${error}`);
       }
     }
+  }
 };
 
 const handleNotificationApi = async () => {
-    try
-    {
-        await askPermission();
-        await registerServiceWorker();
-    } catch(exception)
-    {
-        console.log(exception)
-    }
-}
+  try {
+    await askPermission();
+    await registerServiceWorker();
+  } catch (exception) {
+    console.log(exception);
+  }
+};
 
 const IndexComponent = () => {
-    return (
-        <div>
-            <NavComponent/>
-            <button style={{padding: "20px"}} onClick={handleNotificationApi}>Ask for permission</button>
-        </div>
-    )
-}
+  return (
+    <div>
+      <NavComponent />
+      <button style={{ padding: "20px" }} onClick={handleNotificationApi}>
+        Ask for permission
+      </button>
+    </div>
+  );
+};
 
-export const Index = React.memo(IndexComponent)
+export const Index = React.memo(IndexComponent);
