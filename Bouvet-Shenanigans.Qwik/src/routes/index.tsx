1c546f3b2584b9af707b75cb07e714b2be191537
sivertheisholt <sivert.b.heisholt@protonmail.com>
2023-08-25 11:09:45 +0200

    Trying smth else

--- Bouvet-Shenanigans.Qwik/src/routes/index.tsx
+++ Bouvet-Shenanigans.Qwik/src/routes/index.tsx
@@ -1,15 +1,11 @@
 import { component$ } from "@builder.io/qwik";
 import { routeLoader$, type DocumentHead } from "@builder.io/qwik-city";
+import { customAxios } from "~/api/custom-axios";
 import { OverviewElectricity } from "~/components/electricity/overview-electricity";
 import { OverviewFactory } from "~/components/factory/overview-factory";
 
 import Hero from "~/components/starter/hero/hero";
 import Starter from "~/components/starter/next-steps/next-steps";
-import { fetchCoalPlants } from "~/endpoints/fetch-coal-plants";
-import { fetchFactories } from "~/endpoints/fetch-factories";
-import { fetchSolar } from "~/endpoints/fetch-solar";
-import { fetchUser } from "~/endpoints/fetch-user";
-import { fetchWind } from "~/endpoints/fetch-wind";
 import type { CoalPlant } from "~/types/coal-plant";
 import type { Factory } from "~/types/factory";
 import type { Solar } from "~/types/solar";
@@ -18,26 +14,36 @@ import type { Wind } from "~/types/wind";
 
 export const useCoalPlantsData = routeLoader$(
   async (): Promise<Array<CoalPlant>> => {
-    return await fetchCoalPlants();
+    return await customAxios.get(`api/electricity/coal`).then((resp: any) => {
+      return resp.data;
+    });
   }
 );
 
 export const useFactoriesData = routeLoader$(
   async (): Promise<Array<Factory>> => {
-    return await fetchFactories();
+    return await customAxios.get(`api/factories`).then((resp: any) => {
+      return resp.data;
+    });
   }
 );
 
 export const useSolarData = routeLoader$(async (): Promise<Solar> => {
-  return await fetchSolar();
+  return await customAxios.get(`api/electricity/solar`).then((resp: any) => {
+    return resp.data;
+  });
 });
 
 export const useWindData = routeLoader$(async (): Promise<Wind> => {
-  return await fetchWind();
+  return await customAxios.get(`api/electricity/wind`).then((resp: any) => {
+    return resp.data;
+  });
 });
 
 export const useUserData = routeLoader$(async (): Promise<User> => {
-  return await fetchUser();
+  return await customAxios.get(`api/users`).then((resp: any) => {
+    return resp.data;
+  });
 });
 
 export default component$(() => {
