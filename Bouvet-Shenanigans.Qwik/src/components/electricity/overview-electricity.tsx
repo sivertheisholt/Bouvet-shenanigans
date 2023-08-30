c3e276a82e38715d2250d22b43e0e4935fbc2a3b
sivertheisholt <sivert.b.heisholt@protonmail.com>
2023-08-30 11:12:04 +0200

    Fixed any for now

--- Bouvet-Shenanigans.Qwik/src/components/electricity/overview-electricity.tsx
+++ Bouvet-Shenanigans.Qwik/src/components/electricity/overview-electricity.tsx
@@ -8,7 +8,7 @@ export const OverviewElectricity = component$(() => {
   const coalPlants = useCoalPlantsData();
 
   const coalTotal = coalPlants.value.reduce(
-    (acc, obj) => (obj.active ? acc + obj.production : acc),
+    (acc: any, obj: any) => (obj.active ? acc + obj.production : acc),
     0
   );
 
