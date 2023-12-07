dbeaf81cb1a55be3b4a4a196be98bb22650b7a2d
sivertheisholt <sivert.b.heisholt@protonmail.com>
2023-12-07 10:57:36 +0100

    Added GPT Retrieval

---
+++ Bouvet-Shenanigans.OpenAi/src/Static/smallBagsPacker.ts
@@ -0,0 +1,61 @@
+export const smallBagsPackerParent = {
+	data: [
+		{
+			id: 1,
+			name: "ingenDrift",
+			displayName: "Ingen Drift",
+			description: "",
+			subCategories: [],
+		},
+		{
+			id: 2,
+			name: "planlegging",
+			displayName: "Planlegging",
+			description: "",
+			subCategories: [],
+		},
+		{
+			id: 3,
+			name: "utilgjengelighet",
+			displayName: "Utilgjengelighet",
+			description: "",
+			subCategories: [],
+		},
+		{
+			id: 4,
+			name: "oppstartOgOmleggingPakking",
+			displayName: "Oppstart / Omlegging Pakking",
+			description: "",
+			subCategories: [],
+		},
+		{
+			id: 5,
+			name: "systematiskOgMenneskelig",
+			displayName: "Systematisk / Menneskelig",
+			description: "",
+			subCategories: [],
+		},
+		{
+			id: 6,
+			name: "utstyrOgUpstream",
+			displayName: "Utstyr - Upstream",
+			description: "",
+			subCategories: [],
+		},
+		{
+			id: 7,
+			name: "utstyrOgPakkeanlegg",
+			displayName: "Utstyr - Pakkeanlegg",
+			description: "",
+			subCategories: [],
+		},
+		{
+			id: 8,
+			name: "utstyrOgDownstream",
+			displayName: "Utstyr - Downstream",
+			description: "",
+			subCategories: [],
+		},
+		{},
+	],
+}
