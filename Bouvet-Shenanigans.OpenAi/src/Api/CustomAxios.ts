ef7edb49955adad76c4c678e51e0effa1ef7a58a
sivertheisholt <sivert.b.heisholt@protonmail.com>
2023-12-13 09:28:06 +0100

    Switched to gpt for now

--- Bouvet-Shenanigans.OpenAi/src/Api/CustomAxios.ts
+++ Bouvet-Shenanigans.OpenAi/src/Api/CustomAxios.ts
@@ -1,7 +1,6 @@
 import axios from "axios"
 
-const BASE_URL =
-	"https://bouvetshenanigansopenai-aiservices243105851.openai.azure.com/openai/deployments/gpt-4/chat/completions?api-version=2023-07-01-preview"
+const BASE_URL = "https://api.openai.com/v1/chat/completions"
 
 export const axiosPrivate = axios.create({
 	baseURL: BASE_URL,
