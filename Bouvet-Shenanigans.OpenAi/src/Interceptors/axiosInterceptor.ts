db7c365931cb668e36f3640aed1b6d2f71768929
sivertheisholt <sivert.b.heisholt@protonmail.com>
2023-12-13 09:34:36 +0100

    Added bearer token

--- Bouvet-Shenanigans.OpenAi/src/Interceptors/axiosInterceptor.ts
+++ Bouvet-Shenanigans.OpenAi/src/Interceptors/axiosInterceptor.ts
@@ -3,7 +3,7 @@ import { AxiosInstance } from "axios"
 export const interceptToken = (token: string, axiosInstance: AxiosInstance) => {
 	const requestIntercept = axiosInstance.interceptors.request.use(
 		(config: any) => {
-			config.headers["api-key"] = `${token}`
+			config.headers["Authorization"] = `Bearer ${token}`
 			return config
 		},
 		(error: any) => Promise.reject(error)
